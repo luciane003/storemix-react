2630badb8df96abcaa32552dd87cd9c36ebacad6
luciane003 <lucianekellenpsarges@gmail.com>
2026-05-23 15:06:55 -0300

    corrigir o caminho base do vértice

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/storemix-react/',
+  base: '/',
   plugins: [
     react(), 
     tailwindcss(),
